715a1a189cd16365a80b9952ce6418f1e52e13d4
perseusxiii <briansmcgrail@gmail.com>
2024-09-14 15:29:58 -0400

    Create MovieQuoteInfo.java

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,21 @@
+package moviequoteinfo;
+
+/**
+ *
+ * @author brianmcgrail
+ */
+public class MovieQuoteInfo {
+
+    
+    public static void main(String[] args) {
+        // Prints the favorite quote
+        System.out.println("Yeah, well, you know, that's just, like, your opinion, man.");
+        // Prints the movie name
+        System.out.println("Movie: The Big Lebowski");
+        // Prints the character who said it
+        System.out.println("Character: The Dude");
+        // Prints the year of the movie
+        System.out.println("Year: 1998"); 
+    }
+    
+}
