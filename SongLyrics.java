9feee096072efb6693647542fe095fc306cd2f96
perseusxiii <briansmcgrail@gmail.com>
2024-09-14 15:29:17 -0400

    Create SongLyrics.java

---
+++ SongLyrics.java
@@ -0,0 +1,17 @@
+package songlyrics;
+
+/**
+ *
+ * @author brianmcgrail
+ */
+public class SongLyrics {
+
+    public static void main(String[] args) {
+        // Prints 4 lines of lyrics to the song "Gooey" by Glass Animals
+        System.out.println("Right, my little pooh bear, wanna take a chance?");
+        System.out.println("Wanna sip the smooth air, kick it in the sand");
+        System.out.println("I'd say I told you so but you just gonna cry");
+        System.out.println("You just wanna know those peanut butter vibes");
+    }
+    
+}
